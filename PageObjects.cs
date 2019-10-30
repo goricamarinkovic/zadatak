42c43e861675d00ada36b85ee00662729cb3d444
goricamarinkovic <57160275+goricamarinkovic@users.noreply.github.com>
2019-10-30 20:51:39 +0100

    Add files via upload

---
+++ PageObjects.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Humanity
+{
+    public static class PageObjects
+    {
+        //PROMENLJIVE
+        public static string url = "https://www.humanity.com";
+        public static string txtUsername = "email";
+        public static string txtPassword = "password";
+        public static string btnLogin = "login";
+        public static string txtTitle = "dashwidget_label";
+        public static string msgError = "response-message";
+        public static string btnStaff = "sn_staff";
+        public static string btnAddEmployee = "act_primary";
+        public static string txtFirstname = "_asf1";
+        public static string txtLastname = "_asl1";
+        public static string txtEmail = "_ase1";
+        public static string btnSaveEmployee = "_as_save_multiple";
+        public static string msgEmployee = "_status";
+       
+
+        //SELEKTORI
+        public static string btnLOGIN = "#navbarSupportedContent > div > a:nth-child(3)";
+        public static string btnlogout = "#wrap_us_menu > i";
+        public static string btnSignout = "#userm > div > div:nth-child(13) > a";
+        
+
+
+        //PARAMETRI
+        public static string paramUsername = "goricamarinkovic87@gmail.com";
+        public static string paramPassword = "marinkovic87";
+        public static string invalidUsername = "goricamarinkovic87gmail.com";
+        public static string invalidPassword = "marinkovic";
+        public static string paramFirstname = "Marina";
+        public static string paramFirstname2 = "Snezana";
+        public static string paramFirstname3 = "Una";
+        public static string paramFirstname4 = "Maja";
+        public static string paramFirstname5 = "Sofija";
+        public static string paramLastname = "Markovic";
+        public static string paramLastname2 = "Marinkovic";
+        public static string paramLastname3 = "Medakovic";
+        public static string paramLastname4 = "Gopic";
+        public static string paramEmail = "marina87@gmail.com";
+        public static string paramEmail2 = "gorica87@gmail.com";
+        public static string paramEmail3 = "marinkovic87@gmail.com";
+        public static string invalidEmail = "markovic6gmail.com";
+        public static string numericFirstname = "9876";
+        public static string numericLastname = "5432";
+        public static string numericEmail = "9876@gmail.com";
+        
+        
+    }
+}
