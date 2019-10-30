9af587910c053329b47961b0aecf96b65b1e87dc
goricamarinkovic <57160275+goricamarinkovic@users.noreply.github.com>
2019-10-30 20:48:52 +0100

    Add files via upload

---
+++ EmployeeTest.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Humanity
+{
+    public class EmployeeTest
+    {
+        public static void EmployeeStep(string methodName, string stepName)
+        {
+
+            string name = @"C:\Test-Humanity\";
+            string employeePath = name + "20191030";
+
+            if (!Directory.Exists(name))
+            {
+                Directory.CreateDirectory(name);
+            }
+
+            Directory.CreateDirectory(employeePath);
+
+            using (StreamWriter writer = new StreamWriter(employeePath + @"\" + "TestEmployee.txt", true))
+            {
+                writer.WriteLine(DateTime.Now);
+                writer.WriteLine(methodName + " " + stepName);
+                writer.WriteLine();
+            }
+        }
+        public static void EmployeePassedTest()
+        {
+
+            string name = @"C:\Test-Humanity\";
+            string employeePath = name + "20191030";
+
+            if (!Directory.Exists(name))
+            {
+                Directory.CreateDirectory(name);
+            }
+
+            Directory.CreateDirectory(employeePath);
+
+            using (StreamWriter writer = new StreamWriter(employeePath + @"\" + "TestEmployee.txt", true))
+            {
+                writer.WriteLine("TEST IS PASSED!");
+                writer.WriteLine();
+            }
+        }
+
+        public static void EmployeeFailedTest()
+        {
+
+            string name = @"C:\Test-Humanity\";
+            string employeePath = name + "20191030";
+
+            if (!Directory.Exists(name))
+            {
+                Directory.CreateDirectory(name);
+            }
+
+            Directory.CreateDirectory(employeePath);
+
+            using (StreamWriter writer = new StreamWriter(employeePath + @"\" + "TestEmployee.txt", true))
+            {
+                writer.WriteLine("TEST IS FAILED!");
+                writer.WriteLine();
+            }
+        }
+
+        public static void EmployeeEnd()
+        {
+
+            string name = @"C:\Test-Humanity\";
+            string employeePath = name + "20191030";
+
+            if (!Directory.Exists(name))
+            {
+                Directory.CreateDirectory(name);
+            }
+
+            Directory.CreateDirectory(employeePath);
+
+            using (StreamWriter writer = new StreamWriter(employeePath + @"\" + "TestEmployee.txt", true))
+            {
+                writer.WriteLine("---------------------------------------------------------------------------");
+                writer.WriteLine();
+            }
+        }
+
+    }
+}
